90a92ffe9726d3325e9f8fdc9607a84ab7eb4122
constructman <famelow@gmail.com>
2023-07-06 18:51:56 +0300

    configured nextui

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,19 @@ import Pricing from "../components/Pricing";
 import Hero from "../components/Hero";
 import Layout from "../components/Layout/Layout";
 import SeoHead from "../components/SeoHead";
+import {NextUIProvider} from '@nextui-org/react'
 
 export default function Home() {
   return (
     <>
-      <SeoHead title='LaslesVPN Landing Page' />
-      <Layout>
-        <Hero />
-        <Feature />
-        <Pricing />
-      </Layout>
+      <NextUIProvider>
+        <SeoHead title='LaslesVPN Landing Page' />
+        <Layout>
+          <Hero />
+          <Feature />
+          <Pricing />
+        </Layout>
+      </NextUIProvider>
     </>
   );
 }
